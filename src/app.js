4a4120ef8cc5b4be39f56fa730ddaf0fd1772eaf
Marinafdez-o <mfz.oria@gmail.com>
2025-06-27 09:49:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,47 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let sustantivos = [
+    "Mi hermano ",
+    "Mi perro ",
+    "Mi gato ",
+    "Mi jefe ",
+    "Mi madre ",
+  ];
+ 
+  let acciones =[
+    "Quemó mi ordenador, ",
+    "Comió mi tarea, ",
+    "Robó mi coche, ",
+    "Se cayó por las escaleras, ",
+    "Derramó mi café, ",
+  ];
+
+  let evitar = [
+    "Estaba distraído ",
+    "No sabía que estaba mal ",
+    "No pude evitarlo ",
+    "No sabía que era ilegal ",
+    "Pensé que era una broma ",
+  ]
+
+  let cuando = [
+    "Ayer.",
+    "La semana pasada.",
+    "El mes pasado.",
+    "El año pasado.",
+    "Hace un rato.",
+  ];
+
+  let parte1 = Math.floor(Math.random() * sustantivos.length);
+  let parte2 = Math.floor(Math.random() * acciones.length);
+  let parte3 = Math.floor(Math.random() * evitar.length);
+  let parte4 = Math.floor(Math.random() * cuando.length);
+
+  document.querySelector("#excusa").innerHTML =
+    sustantivos[parte1] +
+    acciones[parte2] +
+    evitar[parte3] +
+    cuando[parte4];
 };
+
